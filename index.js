df3a0165c4e1c55c79b8b64143a58a5c84b50e51
shreyayaa <104138091+shreyayaa@users.noreply.github.com>
2022-07-07 11:37:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//for dice 1
+var randomNumber1 = (Math.floor(Math.random() * 6)) +1 ;   //Create a random number between 1-6
+var randomImage = "dice" + randomNumber1 + ".png";         //Select image form dice1.png - dice6.png
+var randomImageSource = "images/" + randomImage;          //Select image images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+//for dice 2
+var randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2)
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "  🎉 Player 1 WINS";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 WINS  🎉";
+}
+else{
+    document.querySelector("h1").innerHTML = "It's a tie 💥💥";
+}
+
+
+
